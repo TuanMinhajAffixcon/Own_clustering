9bdb106182e5ec1ae2909ce1f5012f7029323af9
TuanMinhajAffixcon <tuan.seedin@affixcon.com.au>
2024-04-30 16:46:36 +0530

    code updated

--- lookalike.py
+++ lookalike.py
@@ -112,7 +112,7 @@ def spacy_tokenizer(sentence):
     sentence = " ".join(mytokens)
     return sentence
 
-model = SentenceTransformer('all-MiniLM-L6-v2')
+model = SentenceTransformer('all-mpnet-base-v2')
 
 
 # master_transformer=master[['Concatenated']]
